104a368d448b7d27c1d93bc2d5a3c25901a88ea4
kingwoodb <kingwoodb@2bcdb227-e694-438b-b5b4-20df188af6e8>
2014-01-16 20:45:01 +0000

    added Metrics module, and the application is now capturing all pageviews.

---
+++ trunk/themes/ieeecs-HomePage-theme/docroot/_diffs/js/metrics-capture.js
@@ -0,0 +1,108 @@
+/*!
+ * IEEECS Metrics JavaScript SDK
+ * Version: 0.0.1
+ * Built: Wed Nov 13 2013 16:58:16
+ * http://www.computer.org
+ *
+ * Copyright 2013 IEEE Computer Society
+ * The IEEECS Metrics JavaScript SDK is freely distributable under the MIT license.
+ */
+(function( window, undefined ) {
+	var Metrics = {};
+  Metrics.VERSION = "0.0.2";
+	Metrics.ENDPOINT_URL = '';
+	Metrics.DEBUG_MODE = false;
+	
+  // make sure console is available
+  if (!window.console) window.console = {};
+  if (!window.console.log) window.console.log = function () {};
+
+  /**
+   * Contains functions to deal with capturing metric data
+   * @name Metrics.Capture
+   */
+  Metrics.Capture = Metrics.Capture || {};
+	
+  /**
+   * Function that will help determine if the parameter is 
+	 * a function.
+	 * @param optionsOrCallback {}
+   * @name isFunction
+   */
+  function isFunction(functionToCheck) {
+		var getType = {};
+		return functionToCheck && getType.toString.call(functionToCheck) === '[object Function]';
+	}
+	
+  /**
+   * Initializes the Capture object for the Metric API.  As of now
+	 * it simply sets the ENDPOINT_URL.
+	 * @param {Object} options - The initalization options
+   */
+  Metrics.Capture.init = function(options) {
+		Metrics.ENDPOINT_URL = options.url || 'http://localhost:5557';
+		Metrics.DEBUG_MODE = options.debug || false;
+		Metrics.debug("Initializing Metrics SDK.");
+		Metrics.debug("Metrics Server Endpoint URL: " + Metrics.ENDPOINT_URL);
+  };
+	
+  /**
+   * Logs the passed in message to the console.
+	 * @param {String} message - The log message
+   */
+	Metrics.debug = function(message) {
+		if(Metrics.DEBUG_MODE) { console.log(message); }
+	}
+	
+  /**
+   * Run the given callbacks 
+   * @param optionsOrCallback {} A options object or a
+   * callback function. 
+   */
+  Metrics.runCallbacks = function(optionsOrCallback) {
+    var options;
+    if (isFunction(optionsOrCallback)) {
+      return optionsOrCallback;
+    } else {
+      options = optionsOrCallback;
+    }
+    options = options || {};
+		return options;
+  };
+
+  /**
+   * Formats the metric data that will be sent to the Metric API.
+	 * @param {String} name - The name of the event.
+   * @param {Object} data -  The data of the client metrics.  
+   */
+  Metrics.Capture.format = function(name, data) {
+		// add necessary data to json that is needed for analytics
+		var cleanData = {};
+		cleanData.eventName = name;
+		cleanData.data = data;
+		Metrics.debug("Capturing event: " + JSON.stringify(cleanData, null, 4));
+    return cleanData;
+  };
+
+  /**
+   * Sends the metric data to the Metric API.
+	 * @param {String} name - The name of the event.
+   * @param {Object} data -  The data of the client metrics.  
+   * @param {Object} options An object that has an optional success function,
+   * that takes no arguments and will be called on a successful capture, and
+   * an error function that takes a Metrics.Error and will be called if the capture
+   * failed.
+   */
+  Metrics.Capture.capture = function(name, data, options) {		
+    options = options || {};
+		name = name || 'generic';
+		var socket = io.connect(Metrics.ENDPOINT_URL);
+    socket.emit('capture', Metrics.Capture.format(name, data));
+    return Metrics.runCallbacks(options);
+  };
+	
+	// If there is a window object, that at least has a document property, define the Metrics object
+	if ( typeof window === "object" && typeof window.document === "object" ) {
+		window.Metrics = Metrics.Capture;
+	}
+})( window );
